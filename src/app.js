9a86208f5a5242812e2a71cf0c4423411485ccfb
edwardhunton <edward.hunton@rightster.com>
2016-01-24 12:13:40 +0000

    initial view templates and basic routing

--- src/app.js
+++ src/app.js
@@ -1,11 +1,31 @@
 var angular = require('angular');
+require('../styles/vendor/bootstrap/dist/css/bootstrap.min.css');
+
+if(ON_TEST){
+    require('angular-mocks/angular-mocks');
+}
 
 
 
-angular.module('fitwatch', []).controller('MainCtrl',[ function(){
+angular.module('fitwatch', [require('angular-route')]).controller('MainCtrl',[ function(){
 var self = this;
     self.name = "FWatch";
 
 
+}]).config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.when('/', {
+        templateUrl: 'start.html'
+    }).when('/climbing', {
+        templateUrl: 'subcategories.html'
+    }).when('/cycling', {
+        templateUrl: 'subcategories.html'
+    }).when('/swimming', {
+        templateUrl: 'subcategories.html'
+    }).otherwise({redirectTo: '/'});
+
 }]);
 
+// Templates
+require('ng-cache!./templates/views/choose.html');
+require('ng-cache!./templates/views/subcategories.html');
+require('ng-cache!./templates/views/start.html');
--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,20 @@ var config = {
         path: __dirname + '/dist',
         filename: 'bundle.js'
     },
+    module: {
+        loaders: [
+            { test: /\.css$/, loader: 'style-loader!css-loader' },
+            { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file" },
+            { test: /\.(woff|woff2)$/, loader:"url?prefix=font/&limit=5000" },
+            { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/octet-stream" },
+            { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=image/svg+xml" }
+        ]
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            ON_TEST: process.env.NODE_ENV === 'test'
+        })
+    ]
 
 
 }
