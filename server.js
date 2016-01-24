e692daa230e695edf4923a34fca7670086a04f8d
edwardhunton <edward.hunton@rightster.com>
2016-01-24 10:16:18 +0000

    server.js provides entry point, express serves SPA from dist

--- server.js
+++ server.js
@@ -1,39 +1,18 @@
 require('newrelic');
 
-var http = require('http'),
-    fs = require('fs');
+var express = require('express');
+var app = express();
 
+app.set('port', process.env.PORT || 3000);
 
-function serveStaticFile(res, path, contentType, responseCode) {
-    if (!responseCode) responseCode = 200;
-    fs.readFile(__dirname + path, function (err, data) {
-        if (err) {
-            res.writeHead(500, {'Content-Type': 'text/plain'});
-            res.end('500 - Internal Error');
-        } else {
-            res.writeHead(responseCode,
-                {'Content-Type': contentType});
-            res.end(data);
-        }
-    });
-}
+app.use(express.static(__dirname + '/dist'));
 
-var port = process.env.PORT || 3000;
-http.createServer(function(req,res){
-// normalize url by removing querystring, optional // trailing slash, and making lowercase
-    var path = req.url.replace(/\/?(?:\?.*)?$/, '')
-        .toLowerCase(); switch(path) {
-        case '':
-            serveStaticFile(res, '/dist/index.html', 'text/html'); break;
-        case '/about':
-            serveStaticFile(res, '/dist/index.html', 'text/html'); break;
-        case '/img/logo.jpg':
-            serveStaticFile(res, '/dist/index.html', 'text/html'); break;
+app.get('/', function(req, res) {
 
-        default:
-            serveStaticFile(res, '/public/404.html', 'text/html',
-                404); break;
-    }
-}).listen(port);
-console.log('Server started on localhost:'+port+'; press Ctrl-C to terminate....');
+    res.render('dist/index');
+});
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+});
 
