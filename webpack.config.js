85a86780b4549993fb5da51b9867c230238b4ad7
edwardhunton <edward.hunton@rightster.com>
2016-01-24 00:22:33 +0000

    added webpack, cucumber

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+var webpack = require('webpack');
+
+module.exports = {
+    context: __dirname + '/',
+    entry: './src/app.js',
+    output: {
+        path: __dirname + '/dist',
+        filename: 'bundle.js'
+    },
+
+
+}
