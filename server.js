9e0f60b30bbb4e9db9d92570da5d1b4495b1324c
edwardhunton <edward.hunton@rightster.com>
2016-01-25 21:27:33 +0000

    routing between sections and notes, videos and links pages

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ require('newrelic');
 var express = require('express');
 var app = express();
 
+var appHelpers = {};
+
 app.set('port', process.env.PORT || 3000);
 
 app.use(express.static(__dirname + '/dist'));
@@ -10,6 +12,12 @@ app.use(express.static(__dirname + '/dist'));
 app.get('/', function(req, res) {
 
     res.render('dist/index');
+});
+
+app.get('*', function (req, res){
+
+    appHelpers.sendFile('dist/index.html', res);
+
 });
 
 app.listen(process.env.PORT || 3000, function () {
